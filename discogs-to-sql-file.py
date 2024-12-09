9b4909a0ff6dfe5bf2784238c7e95347997b85a4
dfwagoner <wagonerd@gmail.com>
2024-12-09 16:35:25 -0500

    Update discogs-to-sql-file.py

--- discogs-to-sql-file.py
+++ discogs-to-sql-file.py
@@ -1,12 +1,16 @@
 # This script queries my discogs collection and writes the data as sql insert statements to a text file.
 # I backed this up to the older-scripts folder and then re-named it 
 
+import os
 import requests
 import json
 
 url = 'https://api.discogs.com/users/dwagoner/collection/folders/0/releases?sort=artist&page=0&per_page=50'
+token = os.getenv("DISCOGS_TOKEN")
 #url = 'https://api.discogs.com/users/dwagoner/collection/folders/0/releases?sort=artist'
-headers = {"Authorization": "Discogs token=put_token_here"}
+headers = {"Authorization": "Discogs token=" + token}
+
+ 
 
 response = requests.get(url, headers=headers)
 raw_json = response.json()
@@ -14,9 +18,11 @@ raw_json = response.json()
 #for help, watch the "Dictionaries in Python" (lesson 23) in the Udemy python class, at around 6:00
 #print(json_string)
 #print(raw_json['releases'][0]['instance_id'])
+#print(raw_json)
 
 #create pagination variables
 totalpages = (raw_json['pagination']['pages'])
+print("total pages= " + str(totalpages))
 
 #pagination_enum = raw_json
 #for i in range(totalpages):
@@ -27,7 +33,7 @@ count = 0
 
 for p in range(totalpages):
     url = str('https://api.discogs.com/users/dwagoner/collection/folders/0/releases?sort=artist&page=' + str(p) + '&per_page=50')
-    headers = {"Authorization": "Discogs token=KKDmkHVNNaozUzjnoWdAbEAiLjIVFSgMkuQXNFVw"}
+    headers = {"Authorization": "Discogs token=" + token}
     response = requests.get(url, headers=headers)
     raw_json = response.json()   
     #print("p is " + str(p))
@@ -38,27 +44,29 @@ for p in range(totalpages):
             if len(raw_json['releases'][i]['notes'])==3:
 
                 with open('discogs_data_sql.txt', mode='a', newline='\n') as f:
-                    f.write('INSERT INTO discogs_data VALUES ' + 
+                    f.write('INSERT INTO discogs_data VALUES (' + 
                     str(raw_json['releases'][i]['id']) + ", " + 
                     str(raw_json['releases'][i]['instance_id']) + ", " + 
                     str(raw_json['releases'][i]['basic_information']['artists'][0]['id']) + ", " +  "\"" +  
                     raw_json['releases'][i]['basic_information']['title'] + "\"" + ", " +  "\"" + 
                     raw_json['releases'][i]['basic_information']['artists'][0]['name'] + "\"" + ", " +  
                     str(raw_json['releases'][i]['basic_information']['year']) + ", " + str(raw_json['releases'][i]['rating']) + ", " + "\"" + 
-                    raw_json['releases'][i]['notes'][2]['value'] + "\"" + "\n")
+                    raw_json['releases'][i]['notes'][2]['value'] + "\"" + ")\n")
+                    count = count + 1
               
             else:
-                    f.write('INSERT INTO discogs_data VALUES ' +
+                    f.write('INSERT INTO discogs_data VALUES (' +
                     str(raw_json['releases'][i]['id']) + ", " + 
                     str(raw_json['releases'][i]['instance_id']) + ", " + 
                     str(raw_json['releases'][i]['basic_information']['artists'][0]['id']) + ", " +  "\"" +  
                     raw_json['releases'][i]['basic_information']['title'] + "\"" + ", " +  "\"" + 
                     raw_json['releases'][i]['basic_information']['artists'][0]['name'] + "\"" + ", " +  
-                    str(raw_json['releases'][i]['basic_information']['year']) + ", " + str(raw_json['releases'][i]['rating']) + "\"" + ", " + 
-                    "\"\"" + "\n")      
+                    str(raw_json['releases'][i]['basic_information']['year']) + ", " + str(raw_json['releases'][i]['rating']) + ", " + 
+                    "\"\"" + ")\n")
+                    count = count + 1      
     
         #print(dict1)
         #print("i is " + str(i))
-#print ("count is " + str(count))      
+print ("count is " + str(count))      
 
 
